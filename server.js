81c40af6adfd6f29185bbb3cb0279dbef495c28e
Ekampreetkaur03 <ekampreet.03122005@gmail.com>
2026-06-02 21:14:01 +0530

    Change static file serving directory to current directory

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(__dirname));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/queue', queueRouter);
